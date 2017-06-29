e494ea1eb56b1a5e03fe5bf9983d9e4fff192e0d
Gus Galarnyk <gusgalarnyk@gmail.com>
2017-06-28 23:56:53 -0500

    Small Step
    Added the animation and changed how movement worked to feel more precise
    and less physics based. Currently it moves in units that are too large,
    will fix ASAP.

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -2,25 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
 
     private Rigidbody2D rb2d;
 
-	void Start ()
-	{
-		rb2d = GetComponent<Rigidbody2D> ();
-	}
+    Vector2 dest = Vector2.zero;
 
-	void FixedUpdate()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-		rb2d.AddForce (movement * speed);
-	}
-	
+    bool valid(Vector2 dir)
+    {
+        // Cast Line from 'next to player' to 'player'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        dest = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        // Check for Input if not moving
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+    }
+
 	void Update ()
 	{
 		

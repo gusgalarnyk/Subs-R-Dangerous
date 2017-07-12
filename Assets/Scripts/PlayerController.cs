4098f5d8496d1111f7ee2d2e5bf5dbf63af5b0e0
Gus Galarnyk <gusgalarnyk@gmail.com>
2017-07-11 19:34:33 -0500

    Everything but waypoints is working
    gah

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+    private string nextDir;
+    private string lastDir;
+
+    Vector2 dest = Vector2.zero;
+
+    bool valid(Vector2 dir)
+    {
+        // Cast Line from 'next to player' to 'player'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+
+        bool isWall;
+        if (hit.collider.name == "Background")
+        {
+            isWall = false;
+        }
+        else
+        {
+            isWall = true;
+        }
+
+        return (isWall);
+    }
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        dest = transform.position;
+        nextDir = "";
+        lastDir = "";
+    }
+
+    void FixedUpdate()
+    {
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            nextDir = "u";
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            nextDir = "r";
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            nextDir = "d";
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            nextDir = "l";
+        }
+
+        // Check for Input if not moving
+        if ((Vector2)transform.position == dest)
+        {
+            if (nextDir == "u" && valid(Vector2.up))
+            {
+                dest = (Vector2)transform.position + Vector2.up;
+                lastDir = "u";
+            }
+            else if (nextDir == "r" && valid(Vector2.right))
+            {
+                dest = (Vector2)transform.position + Vector2.right;
+                lastDir = "r";
+            }
+            else if (nextDir == "d" && valid(-Vector2.up))
+            {
+                dest = (Vector2)transform.position - Vector2.up;
+                lastDir = "d";
+            }
+            else if (nextDir == "l" && valid(-Vector2.right))
+            {
+                dest = (Vector2)transform.position - Vector2.right;
+                lastDir = "l";
+            }
+            else if (lastDir == "u" && valid(Vector2.up))
+            {
+                dest = (Vector2)transform.position + Vector2.up;
+            }
+            else if (lastDir == "r" && valid(Vector2.right))
+            {
+                dest = (Vector2)transform.position + Vector2.right;
+            }
+            else if (lastDir == "d" && valid(-Vector2.up))
+            {
+                dest = (Vector2)transform.position - Vector2.up;
+            }
+            else if (lastDir == "l" && valid(-Vector2.right))
+            {
+                dest = (Vector2)transform.position - Vector2.right;
+            }
+
+            /* One for One movement
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+            {
+                dest = (Vector2)transform.position + Vector2.up;
+                lastDir = "u";
+            }
+                
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+            {
+                dest = (Vector2)transform.position + Vector2.right;
+                lastDir = "r";
+            }
+                
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+            {
+                dest = (Vector2)transform.position - Vector2.up;
+                lastDir = "d";
+            }
+                
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+            {
+                dest = (Vector2)transform.position - Vector2.right;
+                lastDir = "l";
+            }
+            */
+        }
+    }
+
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/monsterMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterMove : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    int cur = 0;
+
+    public float speed;
+
+	void FixedUpdate ()
+    {
+        Vector2 p = Vector2.MoveTowards(transform.position, waypoints[cur].position, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        float xDiff = transform.position.x - waypoints[cur].position.x;
+        float yDiff = transform.position.y - waypoints[cur].position.y;
+
+        if (Mathf.Abs(xDiff) < 0.008 && Mathf.Abs(yDiff) < 0.008)
+        {
+            float newX = Mathf.Round(transform.position.x);
+            float newY = Mathf.Round(transform.position.y);
+            print(newX);
+            print(newY);
+            transform.position.Set(newX, newY, 0f);
+            print(transform.position);
+            //GetComponent<Rigidbody2D>().MovePosition(moveDamnit);
+            print("This doing anything?");
+        }
+
+        if ((Vector2)transform.position == (Vector2)waypoints[cur].position)
+        {
+            cur++;
+            print("hit it");
+        }
+        else
+        {
+            print("Current position: " + (Vector2)transform.position);
+            print("Waypoint position: " + (Vector2)waypoints[cur].position);
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
